2f0b4ea9a0fc150800a99adb7fe98bb24adb3362
Oleksa <oleksa.antsupov@gmail.com>
2022-07-13 14:30:58 +0300

    render component, delete fixtures

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const products = {
+const product = {
     "id": "76w0hz7015kkr9kjkav",
     "images": [
       "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
@@ -10,30 +10,28 @@ const products = {
     "category": "laptops",
     "brand": "acer"
   };
-
-const rootElement = document.getElementById('root');
-
 class Card extends React.Component {
-  constructor() {
-    super();
-  }
 
-  render () {
+  render() {
     return (
       <div className="product-card">
         <div className="product-card__img-wrapper">
-            <img src="https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg" alt="" className="product-card__img" />
+            <img
+              src={this.props.images}
+              alt=""
+              className="product-card__img"
+            />
           </div>
         <div className="product-card__content">
           <div className="product-card__wrapper">
             <div className="product-card__rating">
-              <div className="product-card__rating-count">2.89</div>
+              <div className="product-card__rating-count">{this.props.rating}</div>
               <i className="icon-star"></i>
             </div>
-            <div className="product-card__price">15999</div>
+            <div className="product-card__price">{this.props.price}</div>
           </div>
-          <div className="product-card__title">Ноутбук Acer Aspire 3 A315-57G-336G Charcoal Black</div>
-          <div className="product-card__category">laptops</div>
+          <div className="product-card__title">{this.props.title}</div>
+          <div className="product-card__category">{this.props.category}</div>
         </div>
         <button className="button" data-element="cardBtn">Add To Cart</button>
       </div>
@@ -41,4 +39,13 @@ class Card extends React.Component {
   }
 }
 
-ReactDOM.render(<Card products />, rootElement);
+ReactDOM.render(
+  <Card
+    images={product.images[0]}
+    title={product.title}
+    rating={product.rating}
+    price={product.price}
+    category={product.category}
+  />,
+  document.getElementById('root')
+);
